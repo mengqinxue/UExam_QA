f8e11ad9e35d17f1a127ebf2e56653523baafd80
Max Meng <maxmeng@Maxs-Laptop.local>
2025-02-03 21:21:07 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+from year3.math import f01_even_and_odd
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    f01_even_and_odd.y3_math_c1_generate_counting_even_odd_question()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
