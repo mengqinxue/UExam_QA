dc358562e48a7d2bf4a63d6d3383803778ae2e53
mengqinxue <qinxue.meng@gmail.com>
2025-02-06 14:26:39 +0800

    add clock class

--- year2/math/f01_time.py
+++ year2/math/f01_time.py
@@ -1,74 +1,110 @@
+import random
 import turtle as ttt
-import time
+from PIL import Image
 
-t = ttt.Turtle()
-t.hideturtle()
-s = ttt.Turtle()
-s.hideturtle()
-s.left(90)
-s.speed(0)
-f = ttt.Turtle()
-f.hideturtle()
-f.left(90)
-f.speed(0)
-m = ttt.Turtle()
-m.hideturtle()
-m.left(90)
-m.speed(0)
+class ClockGenerator():
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.t = ttt.Turtle()
+        self.t.hideturtle()
+        self.s = ttt.Turtle()
+        self.s.hideturtle()
+        self.s.left(90)
+        self.s.speed(0)
+        self.f = ttt.Turtle()
+        self.f.hideturtle()
+        self.f.left(90)
+        self.f.speed(0)
+        self.m = ttt.Turtle()
+        self.m.hideturtle()
+        self.m.left(90)
+        self.m.speed(0)
+        self.correct_time = self.generate_random_time()
+        self.options = self.generate_options()
+        self.draw_clock()
+        self.draw_time()
+        self.display_options()
+        self.save_image()
+
+    def generate_random_time(self):
+        hh = random.randint(1, 12)  # 生成 1 到 12 之间的随机小时
+        mm = random.randint(0, 59)  # 生成 0 到 59 之间的随机分钟
+        return hh, mm
+
+    def generate_options(self):
+        options = [self.correct_time]
+        while len(options) < 4:
+            hh = random.randint(1, 12)
+            mm = random.randint(0, 59)
+            if (hh, mm) not in options:
+                options.append((hh, mm))
+        random.shuffle(options)
+        return options
+
+    def draw_clock(self):
+        ttt.tracer(0)
+        self.t.penup()
+        self.t.seth(90)
+        for i in range(12):
+            self.t.right(360 / 12)
+            self.t.forward(200)
+            self.t.pendown()
+            self.t.write(i + 1, align="center", font=("微软雅黑", 22, 'normal'))
+            self.t.penup()
+            self.t.backward(200)
+        self.t.goto(220, 12)
+        self.t.pendown()
+        self.t.circle(220)  # 绘制半径为200的圆
+        self.t.penup()
+        ttt.update()
+
+    def draw_time(self):
+        hh, mm = self.correct_time
+
+        self.s.clear()
+        self.s.seth(90)
+        self.s.right((hh + (mm / 60)) * 30)
+        self.s.pensize(15)
+        self.s.fd(60)
+        self.s.fd(-60)
+        ttt.update()
+
+        self.f.clear()
+        self.f.seth(90)
+        self.f.right(mm * 6)
+        self.f.pensize(7)
+        self.f.fd(100)
+        self.f.fd(-100)
+        ttt.update()
+
+    def display_options(self):
+        question = []
+        question.append("Please select right time: ")
+        for i, option in enumerate(self.options):
+            hh, mm = option
+            question.append(f"{hh:02}:{mm:02}")
+        correct_answer = self.options.index(self.correct_time) + 1
+        correct_answer = chr(correct_answer + 64)
+        question.append(correct_answer)
+        # print(question)
+        return question
 
 
-def draw_table():
-    ttt.tracer(0)
-    t.penup()
-    t.seth(90)
-    for i in range(12):
-        t.right(360 / 12)
-        t.forward(200)
-        t.pendown()
-        t.write(i + 1, align="center", font=("微软雅黑", 18, 'normal'))
-        t.penup()
-        t.backward(200)
-    ttt.update()
+    def save_image(self):
+        # 获取底层的 Tkinter 画布
+        canvas = ttt.getcanvas()
+        # 保存为 PS 文件
+        ps_file = "../../tmp/clock.ps"
+        canvas.postscript(file=ps_file)
+        # print(f"PostScript 文件已保存到: {ps_file}")
+
+        # 将 PS 文件转换为 PNG 文件（需要安装 Pillow 库）
+        png_file = self.filename
+        img = Image.open(ps_file)
+        img.save(png_file, "png")
+        # print(f"PNG 文件已保存到: {png_file}")
 
 
-def draw_time():
-    localtime = time.localtime(time.time())
-    global __hh
-    __hh = localtime[3] % 12
-    global __ff
-    __ff = localtime[4]
-    global __mm
-    __mm = localtime[5]
-    print(__hh, __ff, __mm)
-
-    s.clear()
-    s.seth(90)
-    s.right((__hh + (__ff / 60)) * 30)
-    s.pensize(15)
-    s.fd(60)
-    s.fd(-60)
-    ttt.update()
-
-    f.clear()
-    f.seth(90)
-    f.right(__ff * 6)
-    f.pensize(7)
-    f.fd(100)
-    f.fd(-100)
-    ttt.update()
-
-    m.clear()
-    m.seth(90)
-    m.right(__mm * 6)
-    m.pensize(3)
-    m.fd(170)
-    m.fd(-170)
-    ttt.update()
-
-
-draw_table()
-
-while True:
-    time.sleep(0.99)
-    draw_time()
-    ttt.update()
+cg = ClockGenerator(filename='../../tmp/clock.png')
+cg.display_options()
